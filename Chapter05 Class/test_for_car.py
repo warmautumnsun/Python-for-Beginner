77bc6c788a9601092e8fe7b2f53d840da6377a9f
pxy <44792743+warmautumnsun@users.noreply.github.com>
2019-12-04 18:32:38 +0800

    Create test_for_car.py

---
+++ Chapter05 Class/test_for_car.py
@@ -0,0 +1,14 @@
+
+# from car import Car
+import car
+
+# my_new_car = Car("audi", "a6", 2019)
+# print(my_new_car.get_descriptive_name())
+#
+# my_new_car.odometer_reading = 23
+# my_new_car.read_odometer()
+
+my_beetle = car.Car('volkswagen', 'beetle', 2016)
+print(my_beetle.get_descriptive_name())
+my_tesla=car.ElectricCar("tesla","model s",2019)
+print(my_tesla.get_descriptive_name())
